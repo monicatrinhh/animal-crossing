e984ed55d9d3a55b2dcb02bcfeae7f1c803417e9
monicatrinhh <trinhminhthao21193@gmail.com>
2021-11-24 12:03:23 -0600

    rezise grid and display grass, rivers

--- sketch.js
+++ sketch.js
@@ -3,23 +3,23 @@
 // November 16th, 2021
 
 let grid;
-let gridSize = 20;
+let gridSize = 25;
 let cellWidth; let cellHeight;
-let widthBuffer; let heightBuffer;
-let grass; 
+let widthBuffer;
+let grass; let grassPale; 
 
 function preload(){
-  // grass = loadImage("assets/background/grass.png");  
+  grass = loadImage("assets/background/grass.png"); 
+  grassPale = loadImage("assets/background/grass2.jpg");   
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  widthBuffer = width/6;
-  heightBuffer = height/6;
+  widthBuffer = width/8;
   grid = createEmptyArray(gridSize,gridSize);
   cellWidth = (width-2*widthBuffer)/gridSize  ;
-  cellHeight = (height- 2*heightBuffer)/gridSize ;
+  cellHeight = height/gridSize ;
 }
 
 function draw() {
@@ -30,13 +30,17 @@ function draw() {
 function displayGrid() {
   for (let y= 0; y<gridSize; y++) {
     for (let x = 0; x<gridSize; x++) {
+      noStroke();
+      rect(x*cellWidth+ widthBuffer, y*cellHeight, cellWidth, cellHeight);
       if (grid[y][x] === 0) {
-        fill("white");
+        image(grassPale,x*cellWidth+ widthBuffer, y*cellHeight, cellWidth, cellHeight);
       }
-      if (grid[y][x] === 1) {
-        fill("black");
+      else if (grid[y][x] === 1) {
+        fill("#2acaea");
       }
-      rect(x*cellWidth+ widthBuffer, y*cellHeight + heightBuffer, cellWidth, cellHeight);
+      else if (grid[y][x] === 2) {
+        image(grass,x*cellWidth+ widthBuffer, y*cellHeight, cellWidth, cellHeight);
+      }
     }
   }
 }
@@ -48,10 +52,13 @@ function createEmptyArray(rows, cols) {
     board.push([]);
     for (let x=0; x<cols; x++) {
       if (random(100) < 25) {
-        board[y].push(1);
+        board[y].push(1); // river
+      }
+      else if (random(100) < 50) {
+        board[y].push(2); // dark green
       }
       else {
-        board[y].push(0);
+        board[y].push(0); // pale green
       }
     }
   }
