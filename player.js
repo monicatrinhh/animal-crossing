1b5ae431deb078b12f5807d42e33c5f868310a5e
monicatrinhh <trinhminhthao21193@gmail.com>
2021-11-26 12:12:16 -0600

    make character move

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-let defaultPlayer;
+let inGrid = true;
 
 
 class Player {
@@ -7,12 +7,31 @@ class Player {
     this.y = y;
     this.dx = 5;
   }
-  display(){
-    image(playerImg, this.x, this.y, cellWidth, cellWidth);
+  display() {
+    image(playerImg, this.x, this.y, cellHeight, cellHeight);
+  }
+  move() {
+    if (inGrid) {
+      if (keyIsDown(87)) { //w
+        this.y += -this.dx;
+      }
+      if (keyIsDown(83)) { //s
+        this.y += this.dx;
+      }
+      if (keyIsDown(68)) { //s
+        this.x += this.dx;
+      }
+      if (keyIsDown(65)) { //s
+        this.x += -this.dx;
+      }
+    }
+
+  }
+
+  insideGrid() {
+    if (this.y > 0 && this.y < height) {
+      return inGrid = true;
+    }
   }
 }
 
-function displayPlayer(){
-  
-  thePlayer.display();
-}
--- sketch.js
+++ sketch.js
@@ -3,71 +3,38 @@
 // November 16th, 2021
 
 let grid;
-let gridSize = 25;
+let gridSize = 20;
 let cellWidth; let cellHeight;
 let widthBuffer;
-let grass; let grassPale; 
-let playerImg; 
+let grass; let grassPale;
+let playerImg;
 let gridDimensions;
-let thePlayer;
+let defaultPlayer;
 
-function preload(){
-  grass = loadImage("assets/background/grass.png"); 
-  grassPale = loadImage("assets/background/grass2.jpg");  
-  playerImg = loadImage("assets/player/playerMale.png"); 
+
+function preload() {
+  grass = loadImage("assets/background/grass.png");
+  grassPale = loadImage("assets/background/grass2.jpg");
+  playerImg = loadImage("assets/player/male.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  widthBuffer = width/8;
-  grid = createEmptyArray(gridSize,gridSize);
-  cellWidth = (width-2*widthBuffer)/gridSize  ;
-  cellHeight = height/gridSize ;
-  thePlayer = new Player(width-widthBuffer-gridDimensions, cellHeight);
+  widthBuffer = width / 8;
+  grid = createEmptyArray(gridSize, gridSize);
+  cellWidth = (width - 2 * widthBuffer) / gridSize;
+  cellHeight = height / gridSize;
+  defaultPlayer = new Player(widthBuffer, 0);
 }
 
 function draw() {
   background("#2acaea");
   displayGrid();
-  displayPlayer();
+
+  defaultPlayer.move();
+  defaultPlayer.display();
+
 }
 
-function displayGrid() {
-  for (let y= 0; y<gridSize; y++) {
-    for (let x = 0; x<gridSize; x++) {
-      noStroke();
-      rect(x*cellWidth+ widthBuffer, y*cellHeight, cellWidth, cellHeight);
-      if (grid[y][x] === 0) {
-        image(grassPale,x*cellWidth+ widthBuffer, y*cellHeight, cellWidth, cellHeight);
-      }
-      else if (grid[y][x] === 1) {
-        fill("#2acaea");
-      }
-      else if (grid[y][x] === 2) {
-        image(grass,x*cellWidth+ widthBuffer, y*cellHeight, cellWidth, cellHeight);
-      }
-    }
-  }
-}
-
-
-function createEmptyArray(rows, cols) {
-  let board = [];
-  for (let y=0; y<rows; y++) {
-    board.push([]);
-    for (let x=0; x<cols; x++) {
-      if (random(100) < 20) {
-        board[y].push(1); // river
-      }
-      else if (random(100) < 50) {
-        board[y].push(2); // dark green
-      }
-      else {
-        board[y].push(0); // pale green
-      }
-    }
-  }
-  return board;
-}
 
